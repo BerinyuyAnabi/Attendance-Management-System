4c1cb4da9280a50c3e8d0966a0528fb715d05b98
Vanessa Logan Berinyuy Anabi <vanessalogan@Vanessas-MacBook-Air.local>
2025-11-23 19:07:12 +0000

    Made the connect_db to work with the env file

--- db/connect_db.php
+++ db/connect_db.php
@@ -1,7 +1,7 @@
-<?php
 
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
+<?php
+// Import environment variables from connect.env
+$env = parse_ini_file('/Applications/MAMP/htdocs/Attendance-Management-System/env/connect.env');// if your connect is in
 
 // Collect the data from the form
 
@@ -9,22 +9,21 @@ $firstName = $_GET['firstname'];
 $lastName  = $_GET['lastname'];
 $email     = $_GET['email'];
 $role      = $_GET['role'];
-$password  = $_GET['password']; /
-
-// connect to the database
-$servername = "localhost";
-$username = "root";
-$dbpassword = "root";
-$dbname = "Attendance_Management_System";
-
-// Creating a database connection
-$conn = new mysqli($servername, $username, $dbpassword, $dbname);
-if($conn->connect_error){
-    die("Connection failed: " . $conn->connect_error);
+$password  = $_GET['password']; 
+// Use the values from the environment file to connect
+$conn = new mysqli(
+$env['host'],
+$env['user'],
+$env['password'],
+$env['database']
+);
+// Check connection
+if ($conn->connect_error) {
+die("Connection failed: " . $conn->connect_error);
 }
-else {
+else{
     echo "Connected successfully \n";
     // <br>
 }
-
 ?>
+
