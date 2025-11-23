f142c31337436815ffb2feaace5d28c154b3d056
Vanessa Logan Berinyuy Anabi <vanessalogan@Vanessas-MacBook-Air.local>
2025-11-23 22:45:33 +0000

    Updated css link

--- signup/signup.php
+++ signup/signup.php
@@ -10,7 +10,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Signup Page</title>
     <!-- <link rel="stylesheet" href="/Attendance-Management-System/login/login.css"> -->
-    <link rel="stylesheet" href="/Attendance-Management-System/login/login.css">
+    <link rel="stylesheet" href="../login/login.css">
 </head>
 <body>
 
