720c9fe65da521029ffb5395b36d2d3294580c1f
Vanessa Logan Berinyuy Anabi <vanessalogan@Vanessas-MacBook-Air.local>
2025-11-23 22:17:48 +0000

    Fixed the index page

--- index.php
+++ index.php
@@ -6,19 +6,19 @@ if(isset($_SESSION['logged_in']) && $_SESSION['logged_in'] === true) {
     $role = $_SESSION['role'];
 
     if($role === 'student') {
-        header("Location: studentdashboard.php");
+        header("Location: dashboard/studentdashboard.php");
         exit();
     } elseif($role === 'faculty') {
-        header("Location: facultydashboard.php");
+        header("Location: dashboard/facultydashboard.php");
         exit();
     } elseif($role === 'student') {
-        header("Location: studentdashboard.php");
+        header("Location: dashboard/studentdashboard.php");
         exit();
     }
     exit();}
     else {
 
-    header("Location: signin.php");
+    header("Location: login/signin.php");
     exit();
 }
 ?>
